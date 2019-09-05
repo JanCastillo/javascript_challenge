752d2b717b0e070cfb7b448702a8cc34e9e3c8a5
Jpkemperor <jan_jacm123@hotmail.com>
2019-09-05 14:46:17 -0500

    implemented date search

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-// from data.js
-var tableData = data;
+var button= d3.select("#filter-btn");
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+data.map((sight) => {
+    let row = tbody.append("tr");
+    Object.entries(sight).forEach(([key, value]) => {
+    let cell = row.append("td");
+    cell.text(value);
+    });
+});
+
+button.on("click", function() {
+    let tbody = d3.select("tbody");
+    tbody.remove();
+    d3.select("#ufo-table").append("tbody");
+    let inputElement = d3.select("#datetime");
+    let inputValue = inputElement.property("value");
+    console.log(inputValue);
+    data.map((sight) => {
+        let x = sight.datetime;
+        if (x === inputValue) {
+            console.log(sight);
+            let tbody = d3.select("tbody");
+            let row = tbody.append("tr");
+            Object.entries(sight).forEach(([key,value]) => { 
+                let cell = row.append("td");
+                cell.text(value);
+            });
+        };
+    });
+});
